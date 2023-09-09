cb8026984a9f9caea36347a20b12a035a9a1bf4c
VolodymyrDerkach97 <bobikderkac@gmail.com>
2023-09-09 11:39:09 +0300

    coppy project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
-import './index.css';
-import { KdmProvider } from 'hooks/useKdm';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "components/App/App";
+import "./index.css";
+import { KdmProvider } from "hooks/useKdm";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <KdmProvider>
     <App />
